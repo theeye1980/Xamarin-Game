69f2cd9ea7661f477471c511eab6c06e4a7487ea
Vycheslav <v.kosarev@list.ru>
2022-04-04 07:29:19 +0300

    Добавьте файлы проекта.

---
+++ Xamarin Game/GameView.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Xamarin_Game
+{
+    internal class GameView : SurfaceView, ISurfaceHolderCallback
+    {
+        public GameView(Context context) : base(context)
+        {
+            //зададим переменную для получения размеров экрана
+            var metrics = Resources.DisplayMetrics;
+        }
+
+        public void SurfaceChanged(ISurfaceHolder holder, [GeneratedEnum] Format format, int width, int height)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void SurfaceCreated(ISurfaceHolder holder)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void SurfaceDestroyed(ISurfaceHolder holder)
+        {
+            //throw new NotImplementedException();
+        }
+        public void Resume() { }
+        public void Pause() { }
+
+       
+    }
+}
---
+++ Xamarin Game/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+
+namespace Xamarin_Game
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, ScreenOrientation = Android.Content.PM.ScreenOrientation.Landscape)]
+    public class MainActivity : AppCompatActivity
+    {
+        GameView gameView;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            //усатановим возможность полноэкранного режима
+            this.Window.AddFlags(Android.Views.WindowManagerFlags.Fullscreen);
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+
+            gameView = new GameView(this);
+
+            SetContentView(gameView);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
