a2c92323c5b2188805f175da99ef08819ad251b1
Vycheslav <v.kosarev@list.ru>
2022-04-05 21:27:22 +0300

    ghj,s

--- Xamarin Game/GameView.cs
+++ Xamarin Game/GameView.cs
@@ -9,16 +9,53 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace Xamarin_Game
 {
-    internal class GameView : SurfaceView, ISurfaceHolderCallback
+    class GameView : SurfaceView, ISurfaceHolderCallback
     {
+        Thread gameThread, renderThread;  //рендер и игра в разных потоках
+        ISurfaceHolder surfacholder; // для добавления колбека
+        bool isRunning;
+        int displayX, displayY;
+        float rX, rY; //величина соотношения экрана
+
         public GameView(Context context) : base(context)
         {
             //зададим переменную для получения размеров экрана
             var metrics = Resources.DisplayMetrics;
+            displayX = metrics.WidthPixels;
+            displayY = metrics.HeightPixels;
+            rX = displayX / 1920f; // если ширина экрана будет больше 1920, то это множитель
+            rY = displayY / 1080f;
+
+            surfacholder = Holder;
+            surfacholder.AddCallback(this);
         }
+
+        override
+        public void Draw(Canvas canvas) { 
+            canvas.DrawColor(Color.Yellow);
+        }
+        public void Run() {
+            Canvas canvas = null;
+            while (isRunning) {
+                if (surfacholder.Surface.IsValid) {
+
+                    canvas = surfacholder.LockCanvas(); //Блокируем для начала изменений
+                    Draw(canvas);
+                    surfacholder.UnlockCanvasAndPost(canvas);
+                }
+                Thread.Sleep(17); //После отрисовки приостанавливаем поток на 17 мс
+            }
+        }
+         public void Update()
+        {
+
+        }
+
+
 
         public void SurfaceChanged(ISurfaceHolder holder, [GeneratedEnum] Format format, int width, int height)
         {
@@ -28,14 +65,38 @@ namespace Xamarin_Game
         public void SurfaceCreated(ISurfaceHolder holder)
         {
             //throw new NotImplementedException();
+            Resume();
         }
 
         public void SurfaceDestroyed(ISurfaceHolder holder)
         {
             //throw new NotImplementedException();
+            Pause();    
         }
-        public void Resume() { }
-        public void Pause() { }
+        public void Resume() {
+            isRunning = true;
+            gameThread = new Thread(new ThreadStart(Update)); //Инициализируем потоки
+            renderThread = new Thread(new ThreadStart(Run));
+
+            gameThread.Start();
+            renderThread.Start();   
+        }
+        public void Pause() {
+
+            bool retry = true;
+            while (retry) {
+                try
+                {
+                    isRunning = false;
+                    gameThread.Join();
+                    renderThread.Join();
+                    retry = false;
+                }
+                catch(Exception e) { Console.WriteLine(e.Message); }
+                
+            }
+        
+        }
 
        
     }
